e993821f961b92446e49fed2d1d4af43467c4a12
met <bizarim82@gmail.com>
2020-02-17 10:51:57 +0900

    Fix: test

--- nodelogic/src/controller/MbController.ts
+++ nodelogic/src/controller/MbController.ts
@@ -33,7 +33,7 @@ export class MbController {
 
     @Get('/close')
     public async close(@Req() request: any, @Res() response: any) {
-        return response.send('<script>window.close();</script>');
+        return response.send('<script>callback()</script>');
     }
 
 }
